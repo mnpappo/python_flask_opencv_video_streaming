d37898485ea219f9584cfef9bc09d2fdfab4d2fc
Md. Nadimozzaman Pappo <mnpappo@gmail.com>
2018-08-18 08:51:27 +0600

    init

---
+++ camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture('http://admin:Apekkhik@007@192.168.1.108/cgi-bin/realmonitor.cgi?action=getStream&channel=1&subtype=1')
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        _, image = self.video.read()
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
